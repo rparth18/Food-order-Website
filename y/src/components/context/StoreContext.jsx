232f9b8edc084450697f47710208eb9604f87005
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-16 02:22:05 +0530

    Update StoreContext.jsx

--- y/src/components/context/StoreContext.jsx
+++ y/src/components/context/StoreContext.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 
 const StoreContextProvider =(props)=>{
     const[cartItems,setCartItems]=useState({});
-    const url ="http://localhost:4000"
+    const url ="https://parth-swart.vercel.app"
     const [token,setToken] =useState();
     const[food_list,setFoodList]=useState([])
     const addToCart= async (itemId)=>{
