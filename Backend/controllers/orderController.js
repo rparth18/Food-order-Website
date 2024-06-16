5f35d4c834256eb61125ea60a66add81639cc27d
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-17 00:33:31 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 const placeOrder = async (req,res)=>{
-    const frontend_url = "https://food-order-website-frontend.vercel.app"
+    const frontend_url = "https://food-order-website-ganq.vercel.app"
    try{
     const newOrder = new orderModel({
         userId:req.body.userId,
