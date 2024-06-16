9d6e7d8424e2fd74e70c7988a14c2c28df5188ab
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-16 20:53:09 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,8 @@ const port = process.env.PORT||4000
 
 //middleware  
 app.use(express.json())
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'https://food-order-website-ganq.vercel.app' }));
 
 
 // db connection 
