0bf434ede2580b26d422fee3662170460b454075
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-16 13:01:49 +0530

    Update FoodItem.jsx

--- y/src/components/FoodItem/FoodItem.jsx
+++ y/src/components/FoodItem/FoodItem.jsx
@@ -9,7 +9,7 @@ const FoodItem = ({id,name,price,description,image}) => {
   return (
     <div className='food-item'>
     <div className='food-item-img-container'>
-        <img className='food-item-image' src={url+"/images/"+image} alt=""/>
+        <img className='food-item-image' src={`${url}/api/food/foodphoto/${id}`} alt=""/>
         {
             !cartItems[id] ? <img className='add' onClick={()=>addToCart(id)}src={assets.add_icon_white} alt=""/>
             : <div className='food-item-counter'>
