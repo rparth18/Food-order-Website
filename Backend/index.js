0d8073bcb14eb640589218b11267fd8d2e373e19
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-16 13:41:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,13 @@ const port = process.env.PORT||4000
 
 //middleware  
 app.use(express.json())
-app.use(cors());
+const corsOptions ={
+    origin:'https://food-order-website-rho.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 
 
 // db connection 
