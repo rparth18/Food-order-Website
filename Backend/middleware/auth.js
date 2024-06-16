a14863f49034b44cea334cce1038b265a628f19c
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-16 22:45:51 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -7,7 +7,7 @@ if(!token)
         return res.json({success:false,message:"Not Authorized Login Again"})
     }
     try{
-       const token_decode = jwt.verify(token,process.env.jWT_SECRET)
+       const token_decode = jwt.verify(token,process.env.JWT_SECRET)
        req.body.userId = token_decode.id;
        next();
     }
