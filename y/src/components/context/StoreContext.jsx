51273592686675fbd259f843653c1fa10afed88f
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-06-18 18:45:44 +0530

    Update StoreContext.jsx

--- y/src/components/context/StoreContext.jsx
+++ y/src/components/context/StoreContext.jsx
@@ -46,7 +46,7 @@ const StoreContextProvider =(props)=>{
     }
 
     const fetchFoodList = async ()=>{
-        const response =await axios.get(url+"/api/food/list");
+        const response =await axios.get(`${url}/api/food/list`);
         setFoodList(response.data.products)
     }
 
