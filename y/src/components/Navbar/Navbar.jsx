afe5cab162c42b9530180c1165652749267aac15
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-07-02 14:10:23 +0530

    Update Navbar.jsx

--- y/src/components/Navbar/Navbar.jsx
+++ y/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = ({setShowLogin}) => {
       </a>
       </ul>
       <div className='navbar-right'>
-        <img src={assets.search_icon} alt=""/>
+{/*         <img src={assets.search_icon} alt=""/> */}
         <div className='navbar-search-icon'>
             <Link to='/cart'> <img src={assets.basket_icon} alt=""/></Link>
             <div className={getTotalCartAmount()===0?"":'dot'}></div>
