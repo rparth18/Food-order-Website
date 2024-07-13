cf69bef2ce09e49e5d31a3fd75ad15bfe4dfa9fd
rparth18 <138583089+rparth18@users.noreply.github.com>
2024-07-13 14:36:48 +0530

    Update assets.js

--- Adminmain/src/assets/assets.js
+++ Adminmain/src/assets/assets.js
@@ -2,7 +2,7 @@ import logo from './food_logo1 (2).jpg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
+import upload_area from './upload_area.png' 
 import parcel_icon from './parcel_icon.png'
  
 export const assets ={
